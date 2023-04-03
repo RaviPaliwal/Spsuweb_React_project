aca1fb5cd57a9210bd6a9cb1b3a39477a8517ae5
Ravi Paliwal <ravipaliwal885078@gmail.com>
2023-04-03 17:51:07 +0530

    Update mainDB.js

--- Backend/mainDB.js
+++ Backend/mainDB.js
@@ -1,7 +1,7 @@
 const DB = require('mongoose');
 DB.set('strictQuery', true);
 // const uri = "mongodb://127.0.0.1:27017/SpsuData"
-const uri = "mongodb+srv://ravipaliwal:Ravi%40885078@spsucseweb.tz2q1bs.mongodb.net/test"
+const uri = "mongodb+srv://ravipaliwal:Ravi%40885078@spsucseweb.tz2q1bs.mongodb.net/csespsu"
 const connect = async ()=>{
     await DB.connect(uri,()=>{
         console.log("Connected To DB Successfully")
